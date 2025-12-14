f3ba2b1ccde8a0de5a4795dff36ba4fd7e7ec4cb
aishanisingh2003 <aishani18122003@gmail.com>
2025-12-14 19:28:41 +0530

    core concepts

---
+++ viteproject1/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+
+function App() {
+  
+  return (
+    <>
+    <Chai/>
+    <h1>you can render even more things , now i understand </h1>
+
+    </>
+  )
+  
+}
+
+export default App
---
+++ viteproject1/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai()
+{
+  return (
+
+    <h3> manupulation is happening in this </h3>
+  )
+}
+
+export default Chai
